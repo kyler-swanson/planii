20d5893b0ddb74accf42234d772aff471bbdbd2f
kyler-swanson <kylerswanson@gmail.com>
2022-02-25 11:19:07 -0500

    Add thing routing

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,7 +5,7 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ success: true, message: '' });
 });
 
-// const Thing = require('../controllers/Thing/Thing.routing');
-// router.use('/', Thing);
+const Thing = require('../controllers/Thing/Thing.routing');
+router.use('/', Thing);
 
 module.exports = router;
